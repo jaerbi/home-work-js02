de67c02f9ab64b1f2fed4f4a4980b058bf36b608
jaerbi <jaerbi42@gmail.com>
2017-06-29 09:04:22 +0000

    practic

--- js/cycleFibonacci.js
+++ js/cycleFibonacci.js
@@ -1,4 +1,53 @@
-var userNumber2 = Number(prompt('Введіть число для обрахунку циклом числа Фібаначі'));
+
+var userNum2 = Number(prompt('Введіть будьласка число Цикл'));
+
+/*function calcRecursFibonacci(userNum) {
+    if (userNum < 2) {
+        return userNum;
+    }
+    else {
+        return calcRecursFibonacci(userNum - 1) + calcRecursFibonacci(userNum - 2);
+    }
+}
+
+document.write(calcRecursFibonacci(userNum2));
+
+*/
+ function calcCycleFibonacci(userNum2) {
+     var a = 1;
+     var b = 1;
+     for (var i = 2; i < userNum2; i++){
+         var c = a + b;
+         a = b;
+         b = c;
+     }
+     return b;
+ }
+
+
+document.write(calcCycleFibonacci(userNum2));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*var userNumber2 = Number(prompt('Введіть число для обрахунку циклом числа Фібаначі'));
 
 function calcCycleFibonacci(userNumber2) {
     var fib1 = 1;
@@ -11,4 +60,4 @@ function calcCycleFibonacci(userNumber2) {
     return fib2;
 }
 
-document.write(calcCycleFibonacci(userNumber2));
+document.write(calcCycleFibonacci(userNumber2));*/
--- js/recursFibonacci.js
+++ js/recursFibonacci.js
@@ -1,4 +1,54 @@
-var userNumber = Number(prompt('Введіть число для Рекурсивного обрахунку Фібаначі'));
+var userNum = Number(prompt('Введіть будьласка число РЕкурсія'));
+
+function calcRecursFibonacci(userNum) {
+    if (userNum < 2) {
+        return userNum;
+    }
+    else {
+        return calcRecursFibonacci(userNum - 1) + calcRecursFibonacci(userNum - 2);
+    }
+}
+
+document.write(calcRecursFibonacci(userNum));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*var userNumber = Number(prompt('Введіть число для Рекурсивного обрахунку Фібаначі'));
 
 function calcRecursFibonacci(userNumber) {
     if (userNumber < 2) {
@@ -9,4 +59,4 @@ function calcRecursFibonacci(userNumber) {
     }
 }
 
-document.write(calcRecursFibonacci(userNumber));
+document.write(calcRecursFibonacci(userNumber));*/
